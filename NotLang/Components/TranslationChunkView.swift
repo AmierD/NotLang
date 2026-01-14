faea1c4f961c27f533839f9eae3ee68b22aaeb16
amierld <amierLdavis@gmail.com>
2026-01-13 22:38:04 -0500

    docs(M): Add documentation for LangPost and TranslationChunk models.

---
+++ NotLang/Components/TranslationChunkView.swift
@@ -0,0 +1,40 @@
+//
+//  TranslationChunkView.swift
+//  NotLang
+//
+//  Created by Amier Davis on 1/13/26.
+//
+
+import SwiftUI
+
+/*
+ TODO:
+  - add animation (potentially scale) when chunk is tapped
+  - add logic for explicit spaces
+ */
+/// A view showcasing a ``TranslationChunk``.
+///
+/// > Important: Spaces are currently to be implemented as implicit (meaning that spaces are not included in a translation chunk, and as a result all translation chunks are given equal spacing between eachother as dictated by the spacing of the view they are in) to expedite shipment of MVP. However, this should be updated to be explicit so that the LLM can (more reliably) decide spacing.
+struct TranslationChunkView: View {
+    let chunk: TranslationChunk
+    @State private var isShowingTranslation = false
+    
+    var body: some View {
+        Text("\(isShowingTranslation ? chunk.translation : chunk.text)")
+            .onTapGesture {
+            withAnimation(.spring) {
+                isShowingTranslation.toggle()
+            }
+        }
+        .padding(.horizontal, 2)
+            .overlay(
+                Rectangle()
+                    .stroke(
+                        Color.black,
+                        style: StrokeStyle(lineWidth: 2, dash: [9, 5])
+                    )
+                    .opacity(0.2)
+            )
+            .foregroundStyle(isShowingTranslation ? .indigo : .black)
+    }
+}
--- NotLang/Data/Models/LangPost.swift
+++ NotLang/Data/Models/LangPost.swift
@@ -7,9 +7,12 @@
 
 import Foundation
 
+/// A model for a NotLang post.
 struct LangPost: Identifiable {
     let id: UUID = UUID()
     
+    /// The username of the author of the post
     let author: String
+    /// The text content of the post, represented as an array of ``TranslationChunk``s.
     let content: [TranslationChunk]
 }
